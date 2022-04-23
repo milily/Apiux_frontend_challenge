e2e039c02a6270a4a33ed3914a6f6c26f89051cb
mrise <mrise@mrise.local>
2022-04-22 22:57:40 -0400

    add icons and material ui component

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -1,12 +1,56 @@
-import React from 'react'
+import React, {Fragment} from 'react'
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import CreateIcon from '@mui/icons-material/Create';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const ListItems = ({items}) => {
-    const list = items.map((text)=>(
-        <div>
-            {text}
-        </div>
-    ))
-    return list
+
+        return(
+            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+            {items.map((value, index) => {
+                const labelId = `checkbox-list-label-${value}`;
+
+                return (
+                <ListItem
+                    key={index}
+                    secondaryAction={
+                        <Fragment>
+                            <IconButton edge="end" aria-label="comments">
+                                <CreateIcon />
+                            </IconButton>
+                            <IconButton edge="end" aria-label="comments">
+                                <DeleteIcon />
+                            </IconButton>
+                        </Fragment>
+                    
+                    }
+                    
+                    disablePadding
+                >
+                    <ListItemButton role={undefined}  dense>
+                    <ListItemIcon>
+                        <Checkbox
+                        edge="start"
+                        
+                        tabIndex={-1}
+                        disableRipple
+                        inputProps={{ 'aria-labelledby': labelId }}
+                        />
+                    </ListItemIcon>
+                    <ListItemText id={labelId} primary={value} />
+                    </ListItemButton>
+                </ListItem>
+                );
+            })}
+        </List>
+        )
+    
 }
 
 export default ListItems
